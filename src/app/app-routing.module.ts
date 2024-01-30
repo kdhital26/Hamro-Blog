6ca753e5b290eb2a39ff0af053e3c501b821a06f
Kabin <sagar.kabin26@gmail.com>
2024-01-30 06:18:42 -0500

    added login forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainModule } from '../app/components/main.module';
 import { ContentComponent } from './content/content.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { CommonBlogComponent } from './shared/common-blog/common-blog.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'shared', component: CommonBlogComponent},
   {path: 'content', component: ContentComponent},
+  {path: 'login', component: LoginComponent},
   {
     path: 'blog',
     loadChildren: () => import('../app/shared/shared.module').then(m => m.SharedModule)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContentComponent } from './content/content.component';
 import { AppMainSharedModule } from './components/shared/common/main-shared.module';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { FooterComponent } from './footer/footer.component';
     SubMenuComponent,
     SharedComponent,
     ContentComponent,
+    LoginComponent,
     // FooterComponent
     
   ],
